821f50141f898525154f2ba14ada536f2141ba51
Luca Marius Cobzaru <e11712226@student.tuwien.ac.at>
2022-12-21 11:09:20 +0100

    STU-1225 remove todo

--- src/imagemaxwidth.js
+++ src/imagemaxwidth.js
@@ -1,7 +1,7 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import ImageMaxWidthEditing from "./imagemaxwidth/imagemaxwidthediting";
 import ImageMaxWidthUi from "./imagemaxwidth/imagemaxwidthui";
-//TODO: merge STU-1225 into testing from this repo and remove this todo
+
 export default class ImageMaxWidth extends Plugin {
 
     static get requires() {
